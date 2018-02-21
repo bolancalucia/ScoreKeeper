590d7aa2c208a52fa18848ed6fc1cf99c6232ffd
Lucia Bolanča <bolancalucia@gmail.com>
2018-02-21 09:54:42 +0100

    updating after review

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -8,14 +8,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    static int scoreTeam1 = 0;
-    static int scoreTeam2 = 0;
+    private static int scoreTeam1 = 0;
+    private static int scoreTeam2 = 0;
     final int _SIX_POINTS = 6;
     final int _THREE_POINTS = 3;
     final int _TWO_POINTS = 2;
     final int _ONE_POINT = 1;
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
         pointAfterTouchDown_team2.setEnabled(false);
         twoPointConversion_team1.setEnabled(false);
         twoPointConversion_team2.setEnabled(false);
+
+        Button reset = findViewById(R.id.reset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetScore(view);
+            }
+        });
     }
 
     @Override
@@ -44,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
         super.onRestoreInstanceState(savedInstanceState);
         scoreTeam1 = savedInstanceState.getInt("scoreTeam1");
         scoreTeam2 = savedInstanceState.getInt("scoreTeam2");
-
     }
 
     public void updateScoreTeam1(int score) {
@@ -55,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
     public void updateScoreTeam2(int score) {
         TextView currentScore = findViewById(R.id.ScoreTeam2);
         currentScore.setText(String.valueOf(score));
-
     }
 
     public void addTouchdown_team1(View view) {
@@ -74,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
         Button twoPointConversion_team1 = findViewById(R.id.twoPointConversion_team1);
         pointAfterTouchDown_team1.setEnabled(false);
         twoPointConversion_team1.setEnabled(false);
-
     }
 
     public void addTwoPointConversion_team1(View view) {
